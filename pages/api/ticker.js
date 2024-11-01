dff12c05c1c563bb1e147c6367d60b305730f97c
IceDev528 <iceprince.L528@gmail.com>
2024-11-02 00:48:12 +0100

    remove bitmart and custom indicator

--- pages/api/ticker.js
+++ pages/api/ticker.js
@@ -1,32 +1,28 @@
-import { applyMiddleware } from '../../lib/middleware';
-import Binance from '../../lib/exchanges/binance';
-import MEXC from '../../lib/exchanges/mexc';
-import KuCoin from '../../lib/exchanges/kucoin';
-import Bitmart from '../../lib/exchanges/bitmart';
+import { applyMiddleware } from "../../lib/middleware";
+import Binance from "../../lib/exchanges/binance";
+import MEXC from "../../lib/exchanges/mexc";
+import KuCoin from "../../lib/exchanges/kucoin";
 
 const binance = new Binance();
 const mexc = new MEXC();
 const kucoin = new KuCoin();
-const bitmart = new Bitmart();
 
 export default async function handler(req, res) {
   await applyMiddleware(req, res);
   try {
     const { symbol, exchange } = req.query;
     if (!symbol || !exchange) {
-      res.status(400).json({ s: 'error', errmsg: 'invalid request payload' });
+      res.status(400).json({ s: "error", errmsg: "invalid request payload" });
       return;
     }
 
     const response =
-      exchange.toLowerCase() === 'binance'
+      exchange.toLowerCase() === "binance"
         ? await binance.ticker24hrs(symbol)
-        : exchange.toLowerCase() === 'mexc'
+        : exchange.toLowerCase() === "mexc"
         ? await mexc.ticker24hrs(symbol)
-        : exchange.toLowerCase() === 'kucoin'
+        : exchange.toLowerCase() === "kucoin"
         ? await kucoin.ticker24hrs(symbol)
-        : exchange.toLowerCase() === 'bitmart'
-        ? await bitmart.ticker24hrs(symbol)
         : { lastPrice: 0, priceChange: 0, priceChangePercent: 0 };
 
     const lastPrice = parseFloat(response.lastPrice);
@@ -35,6 +31,6 @@ export default async function handler(req, res) {
 
     res.status(200).json({ lastPrice, priceChange, priceChangePercent });
   } catch (err) {
-    res.status(500).json({ s: 'error', errmsg: 'Internal Error' });
+    res.status(500).json({ s: "error", errmsg: "Internal Error" });
   }
 }
